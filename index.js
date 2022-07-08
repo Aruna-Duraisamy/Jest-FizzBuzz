f8205da9f1b96645fc3d5ada266d36c848919408
arunaganeshraj <aruna.durai@gmail.com>
2022-07-08 10:09:20 +0100

    add tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* FizzBuzz:
- * Given a range 1 - 100
+ * Given a number
  * Print 'Fizz' for multiples for 3  
  * Print 'Buzz' for multiples for 5
  * Print 'FizzBuzz' for multiples of 3 and 5
--- index.test.js
+++ index.test.js
@@ -1,1 +1,23 @@
 const FizzBuzz = require('./index');
+
+describe('fizzBuzz()', () => {
+    it('returns Fizz for multiples of 3', () => {
+        expect(FizzBuzz(3)).toBe('Fizz');
+        expect(FizzBuzz(9)).toBe('Fizz');
+    });
+
+    it('return Buzz for multiples of 5', () => {
+        expect(FizzBuzz(5)).toBe('Buzz');
+        expect(FizzBuzz(10)).toBe('Buzz');
+    });
+
+    it('return FizzBuzz for mulitples of 3 and 5', () => {
+        expect(FizzBuzz(15)).toBe('FizzBuzz');
+        expect(FizzBuzz(30)).toBe('FizzBuzz');
+    });
+
+    it('Otherwise, return the number', () => {
+        expect(FizzBuzz(2)).toBe('2');
+        expect(FizzBuzz(18)).toBe('18');
+    });
+});
