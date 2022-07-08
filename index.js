5ce74fbebc35ec25f02547e81311baeb48911bf9
arunaganeshraj <aruna.durai@gmail.com>
2022-07-08 09:45:01 +0100

    Initial set of files

---
+++ index.js
@@ -0,0 +1,7 @@
+/* FizzBuzz:
+ * Given a range 1 - 100
+ * Print 'Fizz' for multiples for 3  
+ * Print 'Buzz' for multiples for 5
+ * Print 'FizzBuzz' for multiples of 3 and 5
+ * Otherwise, Print the number
+*/
